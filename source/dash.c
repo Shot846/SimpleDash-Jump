d919c6a3ca8ec1048e01405345ef11b8d090dc4a
Shot846 <30981041+Shot846@users.noreply.github.com>
2020-04-10 09:34:26 +0200

    1.2

--- source/dash.c
+++ source/dash.c
@@ -10,6 +10,9 @@ int x, y, z, xv, yv, zv;
 
 int djump2rnd;
 
+float dashpower;
+int dashpowerup;
+
 //Original code by TerminusEst13, Huge thanks to him!
 //Modified by Shot846
 
@@ -23,71 +26,72 @@ JumpCooldown[PlayerNumber()] = 0;
 Script "ThrustMe" (int angle)
 {
 	
+	dashpower = GetCVar("sv_dashpower");
+	dashpowerup = GetCVar("sv_dashpowerup");
+	
 	if(angle == 8) //Absolute Forward
 	{
-	SetActorVelocity(0,xv + FixedMul(cos(GetActorAngle(0)),GetCVar("shot_dashpowerground")),yv + FixedMul(sin(GetActorAngle(0)),GetCVar("shot_dashpowerground")),0.0,FALSE,FALSE);
+	  SetActorVelocity(0,xv + FixedMul(cos(GetActorAngle(0)),dashpower),yv + FixedMul(sin(GetActorAngle(0)),dashpower),0.0,FALSE,FALSE);
 	}
 	
 	if(angle == 4) //Absolute Left
 	{
-	SetActorVelocity(0,xv + FixedMul(cos(GetActorAngle(0)+16384),GetCVar("shot_dashpowerground")),yv + FixedMul(sin(GetActorAngle(0)+16384),GetCVar("shot_dashpowerground")),0.0,FALSE,FALSE);
+	SetActorVelocity(0,xv + FixedMul(cos(GetActorAngle(0)+16384),dashpower),yv + FixedMul(sin(GetActorAngle(0)+16384),dashpower),0.0,FALSE,FALSE);
 	}
 	
 	if(angle == 2) //Absolute Backward
 	{
-	SetActorVelocity(0,xv + FixedMul(cos(GetActorAngle(0)+32768),GetCVar("shot_dashpowerground")),yv + FixedMul(sin(GetActorAngle(0)+32768),GetCVar("shot_dashpowerground")),0.0,FALSE,FALSE);	
+	SetActorVelocity(0,xv + FixedMul(cos(GetActorAngle(0)+32768),dashpower),yv + FixedMul(sin(GetActorAngle(0)+32768),dashpower),0.0,FALSE,FALSE);	
 	}
 	
 	if(angle == 6) //Absolute Right
 	{
-	SetActorVelocity(0,xv + FixedMul(cos(GetActorAngle(0)+49152),GetCVar("shot_dashpowerground")),yv + FixedMul(sin(GetActorAngle(0)+49152),GetCVar("shot_dashpowerground")),0.0,FALSE,FALSE);
+	SetActorVelocity(0,xv + FixedMul(cos(GetActorAngle(0)+49152),dashpower),yv + FixedMul(sin(GetActorAngle(0)+49152),dashpower),0.0,FALSE,FALSE);
 	}
 	
 	if(angle == 7) //Forward Diagonal Left
 	{
-	SetActorVelocity(0,xv + FixedMul(cos(GetActorAngle(0)+8192),GetCVar("shot_dashpowerground")),yv + FixedMul(sin(GetActorAngle(0)+8192),GetCVar("shot_dashpowerground")),0.0,FALSE,FALSE);
+	SetActorVelocity(0,xv + FixedMul(cos(GetActorAngle(0)+8192),dashpower),yv + FixedMul(sin(GetActorAngle(0)+8192),dashpower),0.0,FALSE,FALSE);
 	}
 	
 	if(angle == 9) //Forward Diagonal Right
 	{
-	SetActorVelocity(0,xv + FixedMul(cos(GetActorAngle(0)+57344),GetCVar("shot_dashpowerground")),yv + FixedMul(sin(GetActorAngle(0)+57344),GetCVar("shot_dashpowerground")),0.0,FALSE,FALSE);
+	SetActorVelocity(0,xv + FixedMul(cos(GetActorAngle(0)+57344),dashpower),yv + FixedMul(sin(GetActorAngle(0)+57344),dashpower),0.0,FALSE,FALSE);
 	}
 	
 	if(angle == 1) //Backward Diagonal Left
 	{
-	SetActorVelocity(0,xv + FixedMul(cos(GetActorAngle(0)+24576),GetCVar("shot_dashpowerground")),yv + FixedMul(sin(GetActorAngle(0)+24576),GetCVar("shot_dashpowerground")),0.0,FALSE,FALSE);
+	SetActorVelocity(0,xv + FixedMul(cos(GetActorAngle(0)+24576),dashpower),yv + FixedMul(sin(GetActorAngle(0)+24576),dashpower),0.0,FALSE,FALSE);
 	}
 	
 	if(angle == 3) //Backward Diagonal Right
 	{
-	SetActorVelocity(0,xv + FixedMul(cos(GetActorAngle(0)+40960),GetCVar("shot_dashpowerground")),yv + FixedMul(sin(GetActorAngle(0)+40960),GetCVar("shot_dashpowerground")),0.0,FALSE,FALSE);
+	SetActorVelocity(0,xv + FixedMul(cos(GetActorAngle(0)+40960),dashpower),yv + FixedMul(sin(GetActorAngle(0)+40960),dashpower),0.0,FALSE,FALSE);
 	}
 	
 	if (CheckInventory("OnTheGround") == 0)			  
 			  {
-			  ThrustThingZ(0,GetCVar("shot_dashpowerairup"),0,1);
+			  ThrustThingZ(0,dashpowerup,0,1);
 			  }
               else 
 			  { 
-			  ThrustThingZ(0,GetCVar("shot_dashpowerairup")/2,0,1);
+			  ThrustThingZ(0,dashpowerup/4,0,1);
 			  }
+
 	
-	Log(i:angle);
-	
-	
-	  if (GetCVar("shot_allowdashsound") == 1)
+	  if (GetCVar("cl_allowdashsound") == 1)
               {
-                if (GetCVar("shot_dashsoundtype") == 0)
+                if (GetCVar("cl_dashsoundtype") == 0)
                 {
                   PlaySound(0,"DASH1",CHAN_AUTO,1.0,FALSE);
                 }
-                if (GetCVar("shot_dashsoundtype") == 1)
+                if (GetCVar("cl_dashsoundtype") == 1)
                 {
                   PlaySound(0,"DASH2",CHAN_AUTO,1.0,FALSE);
                 }
               }
 				        
-              DashCooldown[PlayerNumber()] = GetCVar("shot_dashcooldown");
+              DashCooldown[PlayerNumber()] = GetCVar("sv_dashcooldown");
 	
 
 }
@@ -111,7 +115,7 @@ x = GetActorX(0);
 y = GetActorY(0);
 z = GetActorZ(0) - GetActorFloorZ(0);
 	
-		if (DashCooldown[PlayerNumber()] == 0 && GetCvar("shot_allowdash"))
+		if (DashCooldown[PlayerNumber()] == 0 && GetCvar("sv_allowdash"))
         {
           if (buttons & BT_SPEED)
           {
@@ -147,8 +151,7 @@ z = GetActorZ(0) - GetActorFloorZ(0);
 	DashCooldown[PlayerNumber()]--;
 	Delay(1);
 	}
-
-	delay(1);
+    Delay(1);
 	ACS_NamedExecuteAlways("Dash",0);
 }
 
@@ -161,19 +164,19 @@ Script "DoubleJump" ENTER
 OldButtonz = GetPlayerInput(-1, INPUT_OLDBUTTONS);
 Buttonz = GetPlayerInput(-1, INPUT_BUTTONS);
 
- if (GetActorVelZ(0) <= 8 && !CheckInventory("OnTheGround") && CheckInventory("InTheAir") == 0 && buttonz & BT_JUMP && GetCvar("sv_nojump") == 0 && GetCvar("shot_allowdoublejump"))
+ if (GetActorVelZ(0) <= 8 && !CheckInventory("OnTheGround") && CheckInventory("InTheAir") == 0 && buttonz & BT_JUMP && GetCvar("sv_nojump") == 0 && GetCvar("sv_allowdoublejump"))
         {
-		  if(GetCvar("shot_allowdoublejump"))
+		  if(GetCvar("sv_allowdoublejump"))
 		  {
 			  
 			
-          if (GetCVar("shot_allowdjumpsound") == 1)
+          if (GetCVar("cl_allowdjumpsound") == 1)
           {
-            if (GetCVar("shot_djumpsoundtype") == 0)
+            if (GetCVar("cl_djumpsoundtype") == 0)
             {
               PlaySound(0,"DJUMP1",CHAN_AUTO,1.0,FALSE);
             }
-             if (GetCVar("shot_djumpsoundtype") == 1)
+             if (GetCVar("cl_djumpsoundtype") == 1)
             {
               djump2rnd = random(1, 2);
               if (djump2rnd == 1)
